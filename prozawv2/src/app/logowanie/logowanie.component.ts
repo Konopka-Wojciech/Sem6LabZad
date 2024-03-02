5b0e239850dfe472a1cc35888539acafa8fd1389
Wojciech-Konopka <kontobuty@gmail.com>
2024-03-02 12:59:38 +0100

    Dodanie plików apki

---
+++ prozawv2/src/app/logowanie/logowanie.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-logowanie',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './logowanie.component.html',
+  styleUrl: './logowanie.component.css'
+})
+export class LogowanieComponent {
+formData={
+  username:'',
+  password:''
+};
+logowanie(){
+  if (this.formData.username ==='user' && this.formData.password === "password"){
+    console.log('zalogowano');
+  }else{
+    console.log('error');
+  }
+}
+
+}
