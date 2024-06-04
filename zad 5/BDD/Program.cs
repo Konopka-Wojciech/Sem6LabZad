5f0bf688de567fc4a368c7549805c881c77ffbe1
Konopka-Wojciech <161934780+Konopka-Wojciech@users.noreply.github.com>
2024-06-04 22:43:22 +0200

    zad 5

---
+++ zad 5/BDD/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BDD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ zad 5/BDD/TaxCalc.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BDD
+{
+    public class TaxCalc
+    {
+        public static decimal CalculateTax(decimal income)
+        {
+            const decimal FirstThreshold = 120000m;
+            const decimal FirstRate = 0.17m;
+            const decimal SecondRate = 0.32m;
+
+            if (income <= FirstThreshold)
+            {
+                return income * FirstRate;
+            }
+            else
+            {
+                decimal taxForFirstThreshold = FirstThreshold * FirstRate;
+                decimal taxForExcess = (income - FirstThreshold) * SecondRate;
+                return taxForFirstThreshold + taxForExcess;
+            }
+        }
+    }
+}
